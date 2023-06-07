6e438f1033a7f48935c19beb9ce351bc35beb950
Britt Gresham <britt@brittg.com>
2023-06-07 07:59:37 -0700

    Move docker project to subfolder

--- project/start.sh
+++ project/start.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+echo "== envs in container"
+env
+
+echo "== creating index.html"
 echo "Hello $NAME! <br>This was added by the TFC config sourcer" >> /usr/share/nginx/html/index.html
 
+echo "== starting server..."
 nginx
